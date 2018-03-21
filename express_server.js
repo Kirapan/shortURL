76b3833df474de37d8cc5a042be7e6d03a0758da
Kirapan <pxqpxqpxq@hotmail.com>
2018-03-21 17:05:27 +0000

    added cookies function

--- express_server.js
+++ express_server.js
@@ -1,7 +1,10 @@
-var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 8080;
+const express = require('express');
+
+const PORT = process.env.PORT || 8080;
+const cookieParser = require('cookie-parser');
+const app = express();
 app.set("view engine", "ejs");
+app.use(cookieParser());
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -23,10 +26,12 @@ app.get("/urls", (req, res) => {
   var title = "Your URLs";
   let templateVars = {
     title: title,
-    urls: urlDatabase
+    urls: urlDatabase,
+    username : req.cookies['username']
   };
   //console.log(templateVars);
   res.render("urls_index",templateVars);
+  console.log(templateVars.username);
 });
 
 app.get('/hello', (req, res) => {
@@ -75,6 +80,17 @@ app.get("/urls/:id", (req, res) => {
 app.post('/urls/:id', (req, res) => {
   urlDatabase[req.params.id] = req.body.newURL;
   res.redirect('/urls');
+});
+
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username);
+
+  res.redirect('/urls');
+});
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
